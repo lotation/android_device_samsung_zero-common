33933e1f4753b72a402c31ad4a32eb465c440068
enesuzun2002 <4lukankalar@gmail.com>
2018-02-22 16:26:54 +0000

    zero: Enable HWC Stuff

--- zero-common.mk
+++ zero-common.mk
@@ -187,7 +187,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # HWC
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwc.otf=1 \
-    debug.hwc.winupdate=1
+    debug.hwc.winupdate=1 \
+    debug.hwc.force_gpu=0 \
+    debug.hwc.max_hw_overlays=1 \
+    debug.egl.hw=1 \
+    debug.sf.hw=1 \
+    persist.sys.ui.hw=true \
+    video.accelerate.hw=1 \
+    debug.egl.profiler=1 
 
 # media build properties
 PRODUCT_PROPERTY_OVERRIDES += \
