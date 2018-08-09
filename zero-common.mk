b51c7a0b755546453cb77f270591366ccc7042ed
Lukas0610 <mail@lukasberger.at>
2018-08-09 15:00:45 +0200

    zero: update wpa_supplication configuration
    Change-Id: Iff28821a4b498245ee87c6111ae087b402a9d4ea

--- zero-common.mk
+++ zero-common.mk
@@ -355,6 +355,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/vendor/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/filter_ie:system/etc/wifi/filter_ie
