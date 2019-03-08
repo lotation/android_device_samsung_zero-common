3e21eef91f05528d4fe5cea5ce080d3424aee55a
enesuzun2002 <enesuzun200227@gmail.com>
2019-03-08 14:34:20 +0300

    zero: move wifi configs to /vendor

--- zero-common.mk
+++ zero-common.mk
@@ -359,9 +359,9 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/filter_ie:system/etc/wifi/filter_ie
+	$(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf \
+	$(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
+	$(LOCAL_PATH)/configs/filter_ie:system/vendor/etc/wifi/filter_ie
 
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
