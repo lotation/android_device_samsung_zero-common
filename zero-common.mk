d6f7c062bfc409c3f93c71773282c67d1ab4068b
enesuzun2002 <enesuzun200227@gmail.com>
2019-10-20 06:25:01 +0000

    zero: switch to lineage biometrics fp 2.0 hal

--- zero-common.mk
+++ zero-common.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
     fingerprint.exynos5 \
     libbauthtzcommon_shim \
     android.hardware.biometrics.fingerprint@2.1-impl \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service
 
 # Skeletons
 PRODUCT_PACKAGES += \
